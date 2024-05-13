05caf235b2880d3d977908ee79c01eed0eb43b77
MaxG-Mattos <166611554+MaxG-Mattos@users.noreply.github.com>
2024-05-13 08:48:26 -0300

    Add files via upload

--- assets.py
+++ assets.py
@@ -1,4 +1,15 @@
 menu_inicial = 'Assets_em_questao\\Touhou15_5_LunaticEyesInvisibleFullMoon(ReisenUdongeinInaba).ogg'
+primeira_fase = 'Assets_em_questao\\MoFStage2Theme_RoadoftheApotropaicGod_DarkRoad.ogg'
+segunda_fase = 'Assets_em_questao\\ULiLMokousTheme_ReachfortheMoon_ImmortalSmoke.ogg'
+terceira_fase = 'Assets_em_questao\\DivetoEmergency.ogg'
+quarta_fase = 'Assets_em_questao\\LoLK_Junkos_Theme_Pure Furies_Whereabouts_of_theHeart'
+fase_final = 'Assets_em_questao\\GoldenNocturne.ogg'
+
 musica = {}
 musica['start'] = menu_inicial
-print(musica['start'])
+musica['primeira fase'] = primeira_fase
+musica['segunda fase'] = segunda_fase
+musica['terceira fase'] = terceira_fase
+musica['quarta fase'] = quarta_fase
+musica['boss final'] = fase_final
+print(musica['boss final'])
