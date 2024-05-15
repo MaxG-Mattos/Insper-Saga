b49373ca9c891692515a1acae442e8d8360fbf2c
MaxG-Mattos <166611554+MaxG-Mattos@users.noreply.github.com>
2024-05-15 08:18:30 -0300

    Add files via upload

---
+++ configuracoes.py
@@ -0,0 +1,11 @@
+import pygame
+pygame.font.init()
+TELA = pygame.display.set_mode((750, 750))
+FPS = 60
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+FONT1 = pygame.font.SysFont('arial', 32)
+QUIT = 0
+INIT = -1
+RUNNING1 = 1
+RUNNING2 = 2 
